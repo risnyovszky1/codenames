76a8bf746ead41f2c1dbfd982acf673bbfd7f268
Andras <risnyo96@gmail.com>
2021-11-17 18:38:10 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { Router, Route, Link } from "svelte-navigator";
+
+	// pages
+	import Index from "./pages/Index.svelte";
+	import Generator from "./pages/Generator.svelte";
+	
+	export let menu = 1;
+</script>
+
+<style type="text/scss">
+	@import './scss/app.scss';
+	
+</style>
+
+<Router>
+	<header>
+	  <nav>
+		<Link to="/">Home</Link>
+		<Link to="generator">Generator</Link>
+	  </nav>
+	</header>
+  
+	<main>
+	  <Route path="/">
+		<Index />
+	  </Route>
+  
+	  <Route path="/generator">
+		<Generator />
+	  </Route>
+  
+	</main>
+  </Router>
---
+++ src/pages/Generator.svelte
@@ -0,0 +1,3 @@
+<h1>
+	Generator
+</h1>
